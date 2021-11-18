0132e9307c9abe3ab8594902f9bac188074b68d9
Luigi Cruz <alainluigicruz@gmail.com>
2021-11-18 22:30:14 +0800

    Update dependencies (#14)
    * Update tailwindcss
    
    * Update postcss
    
    * Update autoprefixer
    
    * Update typescript
    
    * Update @types/react @types/react-dom
    
    * Update @types/node
    
    * Update @types/jest
    
    * Update next to 12.0.4
    
    * Enable swcMinify
    
    * Enable esmExternals
    
    * Update eslint-config-next
    
    * Update eslint

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@ module.exports = {
   future: {
     strictPostcssConfiguration: true,
   },
+  swcMinify: true,
   images: {
     domains: ['luigicruz.dev'],
   },
+  experimental: { esmExternals: true },
 }
